0c06118a5b316a32ba70526b58a2209c0c3d4a85
tomkp <tom@tomkp.com>
2015-03-18 22:07:06 +0000

    import instead of require

--- test/Calendar-test.js
+++ test/Calendar-test.js
@@ -1,45 +1,52 @@
-var moment = require('moment');
-var expect = require('expect.js');
-//import expect from 'expect.js';
-var React = require('react/addons');
-var TU = React.addons.TestUtils;
-var Calendar = require('../src/Calendar');
+import moment from 'moment';
+import expect from 'expect.js';
+import React from 'react/addons';
+let { TestUtils } = React.addons;
+import Calendar from '../src/Calendar';
 
 
-describe('Calendar', function() {
+describe('Calendar', () => {
 
     var onSelect = function(date) {
         console.info('onSelect', date);
     };
 
     it('displays the correct year', function() {
-        var calendar = TU.renderIntoDocument(<Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />);
-        var month = TU.findRenderedDOMComponentWithClass(calendar, 'year');
+        var calendar = TestUtils.renderIntoDocument(
+            <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
+        );
+        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'year');
         expect(month.getDOMNode().textContent).to.equal('2015');
     });
 
 
     it('displays the correct month', function() {
-        var calendar = TU.renderIntoDocument(<Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />);
-        var month = TU.findRenderedDOMComponentWithClass(calendar, 'month');
+        var calendar = TestUtils.renderIntoDocument(
+            <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
+        );
+        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
         expect(month.getDOMNode().textContent).to.equal('April');
     });
 
 
     it('should be able to go to previous month', function() {
-        var calendar = TU.renderIntoDocument(<Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />);
-        var previous = TU.findRenderedDOMComponentWithClass(calendar, 'previous');
-        TU.Simulate.click(previous);
-        var month = TU.findRenderedDOMComponentWithClass(calendar, 'month');
+        var calendar = TestUtils.renderIntoDocument(
+            <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
+        );
+        var previous = TestUtils.findRenderedDOMComponentWithClass(calendar, 'previous');
+        TestUtils.Simulate.click(previous);
+        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
         expect(month.getDOMNode().textContent).to.equal('March');
     });
 
 
     it('should be able to go to next month', function() {
-        var calendar = TU.renderIntoDocument(<Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />);
-        var next = TU.findRenderedDOMComponentWithClass(calendar, 'next');
-        TU.Simulate.click(next);
-        var month = TU.findRenderedDOMComponentWithClass(calendar, 'month');
+        var calendar = TestUtils.renderIntoDocument(
+            <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
+        );
+        var next = TestUtils.findRenderedDOMComponentWithClass(calendar, 'next');
+        TestUtils.Simulate.click(next);
+        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
         expect(month.getDOMNode().textContent).to.equal('May');
     });
 });
