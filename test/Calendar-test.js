5aa067c0773929a107a6e9b53ceef06a91fa7190
tomkp <tom@tomkp.com>
2015-04-06 09:40:50 +0100

    es6

--- test/Calendar-test.js
+++ test/Calendar-test.js
@@ -7,82 +7,82 @@ import Calendar from '../src/Calendar';
 
 describe('Calendar', () => {
 
-    var onSelect = function(date) {
+    const onSelect = function(date) {
         //console.info('onSelect', date);
     };
 
     it('displays the correct year', function() {
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
         );
-        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'year');
+        const month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'year');
         expect(month.getDOMNode().textContent).to.equal('2015');
     });
 
 
     it('displays the correct month', function() {
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
         );
-        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
+        const month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
         expect(month.getDOMNode().textContent).to.equal('April');
     });
 
 
     it('should be able to go to previous month', function() {
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
         );
-        var previous = TestUtils.findRenderedDOMComponentWithClass(calendar, 'previous');
+        const previous = TestUtils.findRenderedDOMComponentWithClass(calendar, 'previous');
         TestUtils.Simulate.click(previous);
-        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
+        const month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
         expect(month.getDOMNode().textContent).to.equal('March');
     });
 
 
     it('should be able to go to next month', function() {
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
         );
-        var next = TestUtils.findRenderedDOMComponentWithClass(calendar, 'next');
+        const next = TestUtils.findRenderedDOMComponentWithClass(calendar, 'next');
         TestUtils.Simulate.click(next);
-        var month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
+        const month = TestUtils.findRenderedDOMComponentWithClass(calendar, 'month');
         expect(month.getDOMNode().textContent).to.equal('May');
     });
 
 
     it('should trigger the callback with selected date when clicking a day', function(done) {
-        var callback = function(selectedDate) {
+        const callback = function(selectedDate) {
             expect(moment(selectedDate).format("DD/MM/YYYY")).to.equal('08/04/2015');
             done();
         };
 
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={callback} />
         );
-        var days = TestUtils.scryRenderedDOMComponentsWithClass(calendar, 'Day');
+        const days = TestUtils.scryRenderedDOMComponentsWithClass(calendar, 'Day');
         TestUtils.Simulate.click(days[10]);
     });
 
 
     it('should set selected date to selected', function() {
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
         );
-        var days = TestUtils.scryRenderedDOMComponentsWithClass(calendar, 'Day');
+        const days = TestUtils.scryRenderedDOMComponentsWithClass(calendar, 'Day');
         TestUtils.Simulate.click(days[10]);
-        var selected = TestUtils.findRenderedDOMComponentWithClass(calendar, 'selected');
+        const selected = TestUtils.findRenderedDOMComponentWithClass(calendar, 'selected');
         expect(selected.getDOMNode().textContent).to.equal('8');
     });
 
 
     it('should add class to today', function() {
-        var calendar = TestUtils.renderIntoDocument(
+        const calendar = TestUtils.renderIntoDocument(
             <Calendar date={moment("03/04/2015", "DD/MM/YYYY")} onSelect={onSelect} />
         );
-        var days = TestUtils.scryRenderedDOMComponentsWithClass(calendar, 'Day');
+        const days = TestUtils.scryRenderedDOMComponentsWithClass(calendar, 'Day');
         TestUtils.Simulate.click(days[10]);
-        var selected = TestUtils.findRenderedDOMComponentWithClass(calendar, 'selected');
+        const selected = TestUtils.findRenderedDOMComponentWithClass(calendar, 'selected');
         expect(selected.getDOMNode().textContent).to.equal('8');
     });
 
