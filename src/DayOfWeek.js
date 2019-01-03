3f79a1c79830cffe058357fa113e2dabc0ba2620
fad <fnassiri@vente-privee.com>
2019-01-03 14:46:56 +0100

    feat(dayOfWeek): add format prop in DayOfWeek component

--- src/DayOfWeek.js
+++ src/DayOfWeek.js
@@ -2,9 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import moment from 'moment';
 
-const DayOfWeek = ({ date }) => (
-  <th className="DayOfWeek">{date.format(format)}</th>
-);
+const DayOfWeek = (props) => {
+  const { date, format } = props;
+
+  return (
+    <th className="DayOfWeek">
+      {date.format(format)}
+    </th>
+  );
+};
 
 DayOfWeek.propTypes = {
   date: PropTypes.instanceOf(moment).isRequired,
